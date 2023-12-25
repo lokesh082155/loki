439461f72ae475e8bca2e2e9f5bbef83caa88f0a
lokesh082155 <lokeshhachiko@gmail.com>
2023-12-25 17:19:26 +0530

    loo

--- src/main/java/com/registry/ServiceRegistryApplication.java
+++ src/main/java/com/registry/ServiceRegistryApplication.java
@@ -10,6 +10,5 @@ public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceRegistryApplication.class, args);
-		System.err.println("lokesh1");
 	}
 }
